b3c5ddd5349b8ff9033f679645382bf5d75e1499
Debjyoti Banerjee <debjyotibanerjee.db@gmail.com>
2021-10-04 17:53:08 +0530

    Initial Commit (#1)
    Initial Commit of the files

---
+++ GenericMediator/GenericMediator/Controllers/MediatorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+
+namespace GenericMediator.Controllers
+{
+    public class MediatorController : ApiController
+    {
+        #region Global Variables
+        private const HttpStatusCode oK = HttpStatusCode.OK;
+        private const HttpStatusCode iSE = HttpStatusCode.InternalServerError;
+        #endregion
+
+        
+
+        
+    }
+}
